6167433efa0f875aea995b439089aa04f253ee6a
wardal <wardal@users.noreply.github.com>
2017-10-20 15:59:03 +0300

    Check if django-cache used
    Check if django-cache used for results backend and don't import DatabaseBackend.

--- django_celery_results/backends/__init__.py
+++ django_celery_results/backends/__init__.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, unicode_literals
+from django.conf import settings
 
-from .cache import CacheBackend
-from .database import DatabaseBackend
+if settings.CELERY_RESULT_BACKEND == 'django-cache':
+    from .cache import CacheBackend
+else:
+    from .database import DatabaseBackend
 
 __all__ = ['CacheBackend', 'DatabaseBackend']
