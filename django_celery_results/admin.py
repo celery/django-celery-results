544db9d2f507c604fd6fc165e2ee459c15783c60
wardal <igor.dubovik@outlook.com>
2017-10-26 18:21:10 +0300

    Add admin filter to task results https://github.com/celery/django-celery-beat/issues/64

--- django_celery_results/admin.py
+++ django_celery_results/admin.py
@@ -11,6 +11,7 @@ class TaskResultAdmin(admin.ModelAdmin):
 
     model = TaskResult
     list_display = ('task_id', 'date_done', 'status')
+    list_filter = ('status', 'date_done')
     readonly_fields = ('date_done', 'result', 'hidden', 'meta')
     fieldsets = (
         (None, {
