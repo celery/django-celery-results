59850229643e8666a555818c28f760cb52d09f1b
Christopher Dignam <chris@dignam.xyz>
2018-09-05 23:19:09 -0400

    update settings file

--- t/proj/settings.py
+++ t/proj/settings.py
@@ -82,7 +82,14 @@ DATABASES = {
         'OPTIONS': {
             'timeout': 1000,
         },
-    }
+    },
+    'secondary': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 1000,
+        },
+    },
 }
 
 CACHES = {
