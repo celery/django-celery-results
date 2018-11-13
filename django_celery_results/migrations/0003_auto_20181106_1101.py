bb247561918f078837fc14f85b61c66556009eda
Omer Katz <omer.drow@gmail.com>
2018-11-13 18:17:05 +0200

    Revert app label name in migrations too.

--- django_celery_results/migrations/0003_auto_20181106_1101.py
+++ django_celery_results/migrations/0003_auto_20181106_1101.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('celery_results', '0002_add_task_name_args_kwargs'),
+        ('django_celery_results', '0002_add_task_name_args_kwargs'),
     ]
 
     operations = [
