celery/django-celery-results

commit 64315751dbe7eac8b3276acb8ed56db2add71b74

change double quotes to single quotes for support mysql5.6 query (#110)

django_celery_results/managers.py
@@ -148,8 +148,8 @@ class TaskResultManager(models.Manager):
             # MariaDB and MySQL since 8.0 have different transaction isolation
             # variables: the former has tx_isolation, while the latter has
             # transaction_isolation
-            if cursor.execute('SHOW VARIABLES WHERE variable_name IN '
-                              '("tx_isolation", "transaction_isolation");'):
+            if cursor.execute("SHOW VARIABLES WHERE variable_name IN "
+                              "('tx_isolation", "transaction_isolation');"):
                 isolation = cursor.fetchone()[1]
                 if isolation == 'REPEATABLE-READ':
                     warnings.warn(TxIsolationWarning(W_ISOLATION_REP.strip()))

commit da081ed03957369b06472d40b78a9ff0c261e3d4

Fix bug in warn_if_repeatable_read (introduced in PR #110).

django_celery_results/managers.py
@@ -149,7 +149,7 @@ class TaskResultManager(models.Manager):
             # variables: the former has tx_isolation, while the latter has
             # transaction_isolation
             if cursor.execute("SHOW VARIABLES WHERE variable_name IN "
-                              "('tx_isolation", "transaction_isolation');"):
+                              "('tx_isolation', 'transaction_isolation');"):
                 isolation = cursor.fetchone()[1]
                 if isolation == 'REPEATABLE-READ':
                     warnings.warn(TxIsolationWarning(W_ISOLATION_REP.strip()))