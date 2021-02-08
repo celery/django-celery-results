bb193495bf9cd05c88b2c6506d6fbc7684a0674a
alec troemel <alec@mirusresearch.com>
2021-02-08 09:27:58 -0600

    use custom path converter for task pattern in all paths

--- django_celery_results/urls.py
+++ django_celery_results/urls.py
@@ -6,30 +6,50 @@
     URL  to :func:`~celery.views.task_status`.
 """
 
-from django.conf.urls import url, path
+from django.conf.urls import path, register_converter
 
 from . import views
 
-task_pattern = r'(?P<task_id>[\w\d\-\.]+)'
+
+class TaskPatternConverter:
+    """
+    custom path converter for task & group id's,
+    then are slightly different from the built 'uuid'
+    """
+
+    regex = r'[\w\d\-\.]+'
+
+    def to_python(self, value):
+        """convert url to python value"""
+
+        return str(value)
+
+    def to_url(self, value):
+        """convert python value into url, just a string"""
+
+        return value
+
+
+register_converter(TaskPatternConverter, 'task_pattern')
 
 urlpatterns = [
-    url(
-        r'^%s/done/?$' % task_pattern,
+    path(
+        r'<task_pattern:task_id>/done/',
         views.is_task_successful,
         name='celery-is_task_successful'
     ),
-    url(
-        r'^%s/status/?$' % task_pattern,
+    path(
+        r'<task_pattern:task_id>/status/',
         views.task_status,
         name='celery-task_status'
     ),
     path(
-        r'<str:group_id>/group/done/',
+        r'<task_pattern:group_id>/group/done/',
         views.is_group_successful,
         name='celery-is_group_successful'
     ),
     path(
-        r'<str:group_id>/group/status/',
+        r'<task_patern:group_id>/group/status/',
         views.group_status,
         name='celery-group_status'
     ),
