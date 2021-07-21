6b4c87dbd82b7bf8d36df823452e8895467aca59
Thomas Grainger <tagrain@gmail.com>
2021-07-21 08:27:39 +0100

    manual flake8 fixes

--- setup.py
+++ setup.py
@@ -65,6 +65,7 @@ def add_default(m):
 def add_doc(m):
     return (('doc', m.groups()[0]),)
 
+
 pats = {re_meta: add_default,
         re_doc: add_doc}
 here = os.path.abspath(os.path.dirname(__file__))
@@ -81,8 +82,8 @@ with open(os.path.join(here, NAME, '__init__.py')) as meta_fh:
 # -*- Installation Requires -*-
 
 
-def strip_comments(l):
-    return l.split('#', 1)[0].strip()
+def strip_comments(line):
+    return line.split('#', 1)[0].strip()
 
 
 def _pip_requirement(req):
@@ -93,17 +94,19 @@ def _pip_requirement(req):
 
 
 def _reqs(*f):
-    return [
-        _pip_requirement(r) for r in (
-            strip_comments(l) for l in open(
-                os.path.join(os.getcwd(), 'requirements', *f)).readlines()
-        ) if r]
+    with open(os.path.join(os.getcwd(), 'requirements', *f)) as fp:
+        return [
+            _pip_requirement(r)
+            for r in (strip_comments(line) for line in fp)
+            if r
+        ]
 
 
 def reqs(*f):
     return [req for subreq in _reqs(*f) for req in subreq]
 
 # -*- Long Description -*-
+
 
 if os.path.exists('README.rst'):
     long_description = codecs.open('README.rst', 'r', 'utf-8').read()
@@ -123,6 +126,7 @@ class pytest(setuptools.command.test.test):
     def run_tests(self):
         import pytest
         sys.exit(pytest.main(self.pytest_args))
+
 
 setuptools.setup(
     name=NAME,
