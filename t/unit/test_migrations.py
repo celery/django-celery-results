113ac2e12e7eace5619f8855e0ff69a76b8f7996
Harsh Bhikadia <harsh@bhikadia.com>
2021-09-28 12:36:32 +0530

    [test] `MigrationTests`: calling `makemigrations --check` instead of calling internal logic of `makemigrations` to check if no pending migrations

--- t/unit/test_migrations.py
+++ t/unit/test_migrations.py
@@ -1,11 +1,12 @@
 import os
 
 from django.apps import apps
+from django.core.management import call_command
 from django.db.migrations.autodetector import MigrationAutodetector
 from django.db.migrations.loader import MigrationLoader
 from django.db.migrations.questioner import NonInteractiveMigrationQuestioner
 from django.db.migrations.state import ProjectState
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 from django_celery_results import migrations as result_migrations
 
@@ -24,27 +25,20 @@ class MigrationTests(TestCase):
             msg='Detected migration files with the same migration number')
 
     def test_models_match_migrations(self):
-        """Make sure that no model changes exist.
+        """Make sure that no pending migrations exist for the app.
 
-        This logic is taken from django's makemigrations.py file.
         Here just detect if model changes exist that require
         a migration, and if so we fail.
         """
-        app_labels = ['django_celery_results']
-        loader = MigrationLoader(None, ignore_no_migrations=True)
-        questioner = NonInteractiveMigrationQuestioner(
-            specified_apps=app_labels, dry_run=False)
-        autodetector = MigrationAutodetector(
-            loader.project_state(),
-            ProjectState.from_apps(apps),
-            questioner,
+        call_command(
+            "makemigrations", "django_celery_results", "--check", "--dry-run"
         )
-        changes = autodetector.changes(
-            graph=loader.graph,
-            trim_to_apps=app_labels,
-            convert_apps=app_labels,
-            migration_name='fake_name',
-        )
-        self.assertTrue(
-            not changes,
-            msg='Model changes exist that do not have a migration')
+
+    @override_settings(DEFAULT_AUTO_FIELD='django.db.models.BigAutoField')
+    def test_models_match_migrations_with_changed_default_auto_field(self):
+        """Test with changing default_auto_field.
+
+        This logic make sure that no pending migrations created even if
+        the user changes the `DEFAULT_AUTO_FIELD`.
+        """
+        self.test_no_pending_migrations()
