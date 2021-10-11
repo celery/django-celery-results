a5a456630dbd19e64c75fc86fcdf843c912faa2e
badeendjuh <badeendjuh@email.com>
2021-10-11 14:52:23 +0200

    Use string values for django-chache keys.
    When django cache backend tests the key provided by django_celery_results, It wants to have str type value. 
    byte values result in the following exception being raised:
    
      File "/opt/exaboard/eXaboard/lib/python3.6/site-packages/django/core/cache/backends/base.py", line 287, in memcache_key_warnings
        if ord(char) < 33 or ord(char) == 127:
    TypeError: ord() expected string of length 1, but int found

--- django_celery_results/backends/cache.py
+++ django_celery_results/backends/cache.py
@@ -1,26 +1,30 @@
 """Celery cache backend using the Django Cache Framework."""
+from __future__ import absolute_import, unicode_literals
 
+from django.core.cache import cache as default_cache, caches
+from kombu.utils.encoding import bytes_to_str
 from celery.backends.base import KeyValueStoreBackend
-from django.core.cache import cache as default_cache
-from django.core.cache import caches
 
 
 class CacheBackend(KeyValueStoreBackend):
     """Backend using the Django cache framework to store task metadata."""
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        super(CacheBackend, self).__init__(*args, **kwargs)
 
         # Must make sure backend doesn't convert exceptions to dict.
         self.serializer = 'pickle'
 
     def get(self, key):
+        key = bytes_to_str(key)
         return self.cache_backend.get(key)
 
     def set(self, key, value):
+        key = bytes_to_str(key)
         self.cache_backend.set(key, value, self.expires)
 
     def delete(self, key):
+        key = bytes_to_str(key)
         self.cache_backend.delete(key)
 
     def encode(self, data):
