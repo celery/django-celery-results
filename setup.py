2903bd1b53218cdd44d5e430a5ec61388334e57b
Asif Saif Uddin <auvipy@gmail.com>
2021-12-23 17:30:14 +0600

    update CI with django 4.0 (#272)
    * update classifier
    
    * django 4.0 to CI
    
    * no py37 on django 4.0

--- setup.py
+++ setup.py
@@ -43,8 +43,8 @@ classes = """
     Programming Language :: Python :: Implementation :: PyPy
     Framework :: Django
     Framework :: Django :: 2.2
-    Framework :: Django :: 3.1
     Framework :: Django :: 3.2
+    Framework :: Django :: 4.0
     Operating System :: OS Independent
     Topic :: Communications
     Topic :: System :: Distributed Computing
