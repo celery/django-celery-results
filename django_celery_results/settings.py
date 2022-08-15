a6ea7bd2309d09647e73baf1dbcd427f655c1bcb
Diego Castro <diego.castro@kubicum.com>
2022-08-15 20:42:13 +0200

    [#305]: Added a default callable to `get_callback_function`
    `get_callback_function()` gets a default callback as an arg
    returning explicitely an empty dict.
    
    `get_callback_function()` raises an `ImproperlyConfigured`
    exception when the callback is not callable.
    
    ---
    Resolves celery/django-celery-results#305
    
    Fixes celery/django-celery-results#314

--- django_celery_results/settings.py
+++ django_celery_results/settings.py
@@ -1,16 +1,19 @@
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
 
 def get_callback_function(settings_name, default=None):
     """Return the callback function for the given settings name."""
     
     callback = getattr(settings, settings_name, None)
-    if callback is None:
+    if not callback:
         return default
 
-    if callable(callback):
-        return callback
+    if not callable(callback):
+        raise ImproperlyConfigured(f"{settings_name} must be callable.")
+
+    return callback
 
 extend_task_props_callback = get_callback_function(
-    "CELERY_RESULTS_EXTEND_TASK_PROPS_CALLBACK"
+    "CELERY_RESULTS_EXTEND_TASK_PROPS_CALLBACK", dict
 )
