1dfa0fc1288952b716c4d3d289701261967c10e3
Diego Castro <diego.castro@kubicum.com>
2022-08-16 09:12:57 +0200

    Added newline to the end of

--- django_celery_results/settings.py
+++ django_celery_results/settings.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 def get_callback_function(settings_name, default=None):
     """Return the callback function for the given settings name."""
-    
+
     callback = getattr(settings, settings_name, None)
     if not callback:
         return default
@@ -13,6 +13,7 @@ def get_callback_function(settings_name, default=None):
         raise ImproperlyConfigured(f"{settings_name} must be callable.")
 
     return callback
+
 
 extend_task_props_callback = get_callback_function(
     "CELERY_RESULTS_EXTEND_TASK_PROPS_CALLBACK", dict
