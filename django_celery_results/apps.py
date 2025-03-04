5b9bb9d0a9fee9485ba9650cffb5d8c0526c95a7
Bruno Alla <alla.brunoo@gmail.com>
2025-03-04 19:40:48 +0000

    Make app AutoField configurable on first deployment

--- django_celery_results/apps.py
+++ django_celery_results/apps.py
@@ -5,6 +5,8 @@ from django.utils.translation import gettext_lazy as _
 
 __all__ = ['CeleryResultConfig']
 
+from django_celery_results.conf import app_settings
+
 
 class CeleryResultConfig(AppConfig):
     """Default configuration for the django_celery_results app."""
@@ -12,4 +14,4 @@ class CeleryResultConfig(AppConfig):
     name = 'django_celery_results'
     label = 'django_celery_results'
     verbose_name = _('Celery Results')
-    default_auto_field = 'django.db.models.AutoField'
+    default_auto_field = app_settings.DJANGO_CELERY_RESULTS_DEFAULT_AUTO_FIELD
---
+++ django_celery_results/conf.py
@@ -0,0 +1,43 @@
+"""Application settings."""
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Any
+
+from django.conf import settings as django_settings
+
+# All attributes accessed with this prefix are possible
+# to overwrite through django.conf.settings.
+SETTINGS_PREFIX = "DJANGO_CELERY_RESULTS_"
+
+
+@dataclass(frozen=True)
+class AppSettings:
+    """Proxy class to encapsulate all the app settings.
+
+    This instance should be accessed via the singleton
+    ``django_celery_results.conf.app_settings``.
+
+    You shouldn't have to set any of these yourself, the class checks a Django
+    settings with the same name and use these if defined, defaulting to the
+    values documented here.
+    """
+
+    DJANGO_CELERY_RESULTS_DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+    def __getattribute__(self, __name: str) -> Any:
+        """Check if a Django project settings should override the app default.
+
+        In order to avoid returning any random properties of the Django
+        settings, we first inspect the prefix.
+        """
+        if (
+            __name.startswith(SETTINGS_PREFIX)
+            and hasattr(django_settings, __name)
+        ):
+            return getattr(django_settings, __name)
+
+        return super().__getattribute__(__name)
+
+
+app_settings = AppSettings()
