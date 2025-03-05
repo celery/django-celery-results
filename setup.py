3b26c1070ea683fbd1fdd4306c13cc9a031474cc
Bruno Alla <alla.brunoo@gmail.com>
2025-03-05 07:52:51 +0000

    Drop support for Python 3.8

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ classes = """
     License :: OSI Approved :: BSD License
     Programming Language :: Python
     Programming Language :: Python :: 3
-    Programming Language :: Python :: 3.8
     Programming Language :: Python :: 3.9
     Programming Language :: Python :: 3.10
     Programming Language :: Python :: 3.11
@@ -157,6 +156,7 @@ setuptools.setup(
     platforms=['any'],
     license='BSD',
     classifiers=classifiers,
+    python_requires='>=3.9',
     install_requires=reqs('default.txt'),
     tests_require=reqs('test.txt') + reqs('test-django.txt'),
     cmdclass={'test': pytest},
