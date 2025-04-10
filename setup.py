6ee328d52332558a5da63598529655910ef3c354
Asif Saif Uddin <auvipy@gmail.com>
2025-04-10 13:55:41 +0600

    remove python 3.7 from supported version

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ classes = """
     License :: OSI Approved :: BSD License
     Programming Language :: Python
     Programming Language :: Python :: 3
-    Programming Language :: Python :: 3.7
     Programming Language :: Python :: 3.8
     Programming Language :: Python :: 3.9
     Programming Language :: Python :: 3.10
