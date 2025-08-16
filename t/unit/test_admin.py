d6fb3d52eda4e00cfb8b4174c39ddd67cb6c9994
ClanEver <562211524@qq.com>
2025-08-17 03:25:56 +0800

    fix: conflicts in test_admin and test_migrations

--- t/unit/test_admin.py
+++ t/unit/test_admin.py
@@ -1,9 +1,14 @@
-import pytest
 from celery import uuid
+from django.apps import apps
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import (
+    clear_url_caches,
+    get_resolver,
+    path,
+    reverse,
+)
 
 from django_celery_results.admin import TaskResultAdmin
 from django_celery_results.models import TaskResult
@@ -11,10 +16,9 @@ from django_celery_results.models import TaskResult
 User = get_user_model()
 
 
-@pytest.mark.usefixtures("depends_on_current_app")
 class TaskResultAdminTests(TestCase):
     app_name = "django_celery_results"
-    model_name = "taskresult"
+    model = TaskResult
 
     def setUp(self):
         self.admin_user = User.objects.create_superuser(
@@ -26,27 +30,52 @@ class TaskResultAdminTests(TestCase):
         )
 
     def test_add_view(self):
-        url = reverse(f"admin:{self.app_name}_{self.model_name}_add")
+        url = reverse(
+            f"admin:{self.app_name}_{self.model._meta.model_name}_add"
+        )
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_change_view(self):
         url = reverse(
-            f"admin:{self.app_name}_{self.model_name}_change",
+            f"admin:{self.app_name}_{self.model._meta.model_name}_change",
             args=[self.task_result.id],
         )
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
 
-class TaskResultProxy(TaskResult):
-    class Meta:
-        proxy = True
-        app_label = "django_celery_results"
-
-
-admin.site.register(TaskResultProxy, TaskResultAdmin)
-
-
 class TaskResultProxyAdminTests(TaskResultAdminTests):
-    model_name = "taskresultproxy"
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        class TaskResultProxy(TaskResult):
+            class Meta:
+                proxy = True
+                app_label = "django_celery_results"
+
+        cls.model = TaskResultProxy
+        admin.site.register(TaskResultProxy, TaskResultAdmin)
+
+        # The temporary registration of admin requires refreshing the URL cache
+        # Otherwise, it cannot be resolved
+        default_resover = get_resolver()
+        cls.ori_url_patterns_0 = default_resover.url_patterns[0]
+        get_resolver().url_patterns[0] = path("admin/", admin.site.urls)
+        clear_url_caches()
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+        # Unregister the proxy model
+        admin.site.unregister(cls.model)
+        app_config = apps.get_app_config(cls.app_name)
+        model_name = cls.model._meta.model_name
+        if model_name in app_config.models:
+            del app_config.models[model_name]
+
+        # Restore the original URL patterns
+        get_resolver().url_patterns[0] = cls.ori_url_patterns_0
+        clear_url_caches()
